56567e3593bb9885e6ef97390995c77b888a8a9c
Vy Hoang Cu <vy.hoang.cu@gmail.com>
2020-08-30 19:05:08 -0700

    Logic setup

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
+)
+
+type config struct {
+	Github struct {
+		Password string `yaml:"password"`
+		Owner    string `yaml:"owner"`
+		Repo     string `yaml:"repo"`
+	} `yaml:"github"`
+	Columns []struct {
+		Name string `yaml:"name"`
+	} `yaml:"columns"`
+	SizeLabels []struct {
+		Name string `yaml:"name"`
+	} `yaml:"size_labels"`
+}
+
+func main() {
+	// read from config.yml
+	f, err := os.Open("config.yml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	// declare config object to read values from it
+	var cfg config
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Github Go client setup
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: cfg.Github.Password},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// List projects for given user and repo
+	projects, _, err := client.Repositories.ListProjects(ctx, cfg.Github.Owner, cfg.Github.Repo, nil)
+
+	// Target project id
+	projectID := projects[0].GetID()
+
+	// All columns from target project
+	columns, _, err := client.Projects.ListProjectColumns(ctx, projectID, nil)
+	fmt.Println("All Project Columns: ", columns)
+
+	// Target column names
+	fmt.Println("column names from config: ", cfg.Columns)
+
+	targetColumns := filterTargetColumns(cfg, columns)
+	fmt.Println("targetColumns: ", targetColumns)
+
+	// All cards from target columns
+	cardsForTargetColumns := make([]*github.ProjectCard, 0)
+
+	for i := 0; i < len(targetColumns); i++ {
+		cards, _, err := client.Projects.ListProjectCards(ctx, targetColumns[i].GetID(), nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for j := 0; j < len(cards); j++ {
+			cardsForTargetColumns = append(cardsForTargetColumns, cards[j])
+		}
+	}
+
+	// Getting labels
+	labels, _, err := client.Issues.GetLabel(ctx, cfg.Github.Owner, cfg.Github.Repo, cfg.SizeLabels[0].Name)
+	fmt.Println(labels)
+
+}
+
+func filterTargetColumns(conf config, columns []*github.ProjectColumn) []*github.ProjectColumn {
+	fmt.Println("Number of target columns from config: ", len(conf.Columns))
+
+	result := make([]*github.ProjectColumn, 0, len(conf.Columns))
+
+	for i := 0; i < len(columns); i++ {
+		for j := 0; j < len(conf.Columns); j++ {
+			if columns[i].GetName() == conf.Columns[j].Name {
+				// fmt.Println(columns[i].GetName(), " == ", conf.Columns[j].Name)
+				result = append(result, columns[i])
+			}
+		}
+	}
+
+	return result
+}
